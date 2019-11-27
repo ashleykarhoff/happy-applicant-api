9c41bca3b79aa6e8214f1e4504bd836184deb8c3
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-11-27 15:42:10 -0500

    Configure API Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :cards
-  resources :board_columns
-  resources :applicants
+  namespace :api do
+    namespace :v1 do
+      resources :cards
+      resources :board_columns
+      resources :applicants
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
